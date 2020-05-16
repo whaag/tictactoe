afcc504f4f55b5e3f35b99a5d48bcf7a55562e9d
Eduardo Athayde <eduardo.a.athayde@gmail.com>
2020-05-16 23:01:42 +0100

    create a simple makefile

---
+++ src/tictactoe.c
@@ -0,0 +1,14 @@
+/**
+ * Author: Eduardo Athayde
+ * Date: May 2020
+ * Purpose: Main loop
+ * Language: C
+ */
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+int main(void) {
+
+  return EXIT_SUCCESS;
+}
