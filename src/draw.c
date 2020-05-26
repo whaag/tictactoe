acd2e22deb1e73501806afa2b7d57f583115633a
Eduardo Athayde <eduardo.a.athayde@gmail.com>
2020-05-26 21:15:15 +0100

    creates a window

--- src/draw.c
+++ src/draw.c
@@ -1,1 +1,40 @@
+/**
+ * Author: Eduardo Athayde
+ * Date: May 2020
+ * Purpose: Draws to the screen
+ * Language: C
+ */
 
+#include <SDL2/SDL.h>
+#include "draw.h"
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 1024
+
+SDL_Window* window;
+
+Response initialiseDraw() {
+  Response response;
+  
+  window = SDL_CreateWindow("Titactoe",
+                            SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED,
+                            WINDOW_WIDTH,
+                            WINDOW_HEIGHT,
+                            0);
+  if (window) {                          
+    response.code = 0;
+    puts("Window initalised succsessfuly");
+  } else {
+    response.code = 9;
+    strcpy(response.message, "Error initialising Window");
+    fprintf(stderr, "%s \n", SDL_GetError());
+  }
+  
+  return response;
+}
+
+void cleanUpDraw() {
+  SDL_DestroyWindow(window);
+}
+  
